7fd4ee0f44be0b047e164d60270dfeef98c8971d
Matthew Garry <mhgarry92@gmail.com>
2023-10-10 22:40:44 -0400

    create user registration

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,25 @@
+const { Users } = require('../models/User');
+const { hashPass } = require('../middleware/hashPassword');
+const router = require('express').Router();
+
+//user registration post
+router.post('/register', async(req, res) => {
+    //if email and password are missing or incorrectly inputted this error message will be shown
+    if (!req.body.email || !req.body.password) {
+        res.json({ success: false, error: 'Please enter a valid email and password'});
+    }
+    try {
+        // await the result of the hashed password from our hash password function
+        const password = await hashPass(req, res);
+        // await email and password to create user
+        await Users.create({
+            email: req.body.email,
+            password: password,
+        })
+        res.json({ success: true, message: `New user ${req.body.email} created!`})
+    } catch(err) {
+        res.json({ success: false, error: err})
+    }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const dbConnect = require('./db/connection');
-const app = express();
+require('dotenv').config();
 
+const app = express();
+app.use(express.json());
+app.use(cors());  
 require('dotenv').config;
 
 const PORT = process.env.PORT || 3700;
 
-app.use(express.json());
-app.use(cors());
 
-
+// to use our express user routes
+const router = require('./routes/userRoutes');
+// test route
 app.get('/', (req, res) => {
-    res.json('this is a test')
-})
+    res.json('this is a test');
+});
 
+app.use(router); // Use the router middleware
+
+// connect to server and open db for server to use
 dbConnect.once('open', () => { 
     app.listen(PORT, () => {
       console.log(`Server is up and running on Port: http://localhost:${PORT}`);
